fb7564b2160bae9904580b04fe7e0d5157cb92fa
samuelbeltran048@gmail.com <samuelbeltran048@gmail.com>
2024-10-02 10:23:34 -0500

    Java terminado

--- logicaFor.java
+++ logicaFor.java
@@ -1,5 +1,45 @@
 import java.util.Scanner;
 public static void main(String[] args) {
-    
-    
+
+    Scanner sc = new Scanner(System.in);
+
+    double edad  = 0, estadoCivil = 0, estatura = 0, sexo = 0, promedioEdad = 0, promedioEstatura = 0, porcentajePersonas = 0, acumuladorEdad = 0, acumuladorEstatura = 0;
+    int contadorPersonas = 0, cantidadPersonas = 3;
+
+    for (int numeroPersona = 1; numeroPersona <= cantidadPersonas; numeroPersona++) {
+
+    System.out.println("Ingrese la edad de la persona " + numeroPersona + ": ");
+    edad = sc.nextDouble();
+
+    System.out.print("Ingrese el estado civil de la persona " + numeroPersona + " (1.Soltero  2.Casado): ");
+    estadoCivil = sc.nextDouble();
+
+    System.out.print("Ingrese la estatura de la persona " + numeroPersona + " en cms: ");
+    estatura = sc.nextDouble();
+
+    System.out.print("Ingrese el sexo de la persona " + numeroPersona + " (1.Hombre  2.Mujer): ");
+    sexo = sc.nextDouble();
+
+    if ((edad >= 18) && (estadoCivil == 1) && (estatura > 170) && (sexo == 1)) {
+        contadorPersonas++;
+        acumuladorEdad += edad;
+        acumuladorEstatura += estatura;
+    }
 }
+
+if (contadorPersonas > 0) {
+    promedioEdad = acumuladorEdad / contadorPersonas;
+    promedioEstatura = acumuladorEstatura / contadorPersonas;
+    porcentajePersonas = (double) contadorPersonas / cantidadPersonas * 100;
+
+    System.out.println("El promedio de edad de las personas que cumplen es: " + promedioEdad + " años");
+    System.out.println("El promedio de estatura de las personas que cumplen es: " + promedioEstatura + " cms");
+    System.out.println("El porcentaje de personas que cumplen con la condición es del " + porcentajePersonas + " %");
+} else {
+    System.out.println("Ninguna persona cumple con las condiciones.");
+}
+
+
+
+}
+    
--- logicaFor.js
+++ logicaFor.js
@@ -1,3 +1,4 @@
+/*
 var edad = 0, estadoCivil = 0, estatura = 0, sexo = 0, promedioEdad = 0, promedioEstatura = 0, porcentajePersonas = 0, contadorPersonas = 0, acumuladorEdad = 0, acumuladorEstatura = 0, cantidadPersonas = 3;
 
 for (let numeroPersona = 0; numeroPersona <= cantidadPersonas; numeroPersona++) {
@@ -10,7 +11,7 @@ for (let numeroPersona = 0; numeroPersona <= cantidadPersonas; numeroPersona++) 
     /* prompt es una ventana emergente para responder el mensaje y parsefloat es para tranformar el mensaje a tipo de mensaje a float
      PD: EL "PARSE" SIRVE PARA CUALQUIER VARIABLE
     */
-
+/*
     if((edad >= 18)&&(estadoCivil==1)&&(estatura>170)&&(sexo==1)){
 
         contadorPersonas = contadorPersonas +1;
@@ -29,3 +30,5 @@ porcentajePersonas = (contadorPersonas / cantidadPersonas)*100;
 alert ("El promedio de edad de las personas que cumplen es: " + promedioEdad + " años");
 alert ("El promedio de estatura de las personas que cumplen es: " + promedioEstatura + " metros");
 alert ("El porcentaje de personas que cumplen con la condicion es del " + porcentajePersonas + " %");
+
+*/
